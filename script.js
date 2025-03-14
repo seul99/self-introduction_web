332ccd19d97be23bef125bbf14ac86b025559d95
sia <piao516@naver.com>
2025-03-14 16:44:43 +0900

    추가 수정

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 슬라이드 이동시 fade-in 효과
 document.addEventListener("DOMContentLoaded", function () {
    const fadeElements = document.querySelectorAll(".fade-in");
 
@@ -12,6 +13,42 @@ document.addEventListener("DOMContentLoaded", function () {
    }, { threshold: 0.2 });
 
    fadeElements.forEach((el) => observer.observe(el));
+
+
+   // food.html 화면 슬라이드 넘기기
+var slides = document.querySelector('.slides'),
+    slide = document.querySelectorAll('.slides li'),
+    currntIdx = 0,
+    slideCount = slide.length,
+    prevBtn = document.querySelector('.prev'),
+    slideWidth = 300,
+    slideMargin = 30,
+    nextBtn = document.querySelector('.next');
+
+// css 에 margin width 설정
+slides.style.width = (slideWidth + slideMargin) * slideCount - slideMargin +'px';
+
+function moveSlide(num){
+    slides.style.left = -num * 330 +'px';
+    currntIdx = num;
+}
+nextBtn.addEventListener('click', function(){
+    if(currntIdx < slideCount - 3){
+        moveSlide(currntIdx + 1);  
+    } else {
+        moveSlide(0);  
+    }
+});
+
+prevBtn.addEventListener('click', function(){
+    if(currntIdx > 0){
+        moveSlide(currntIdx - 1);  
+    } else {
+        alert("처음입니다!");
+    }
+});
+
+
 });
 
 
